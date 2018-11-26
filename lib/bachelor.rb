a6dd7af9e757079e3a997cf3f62f2b9070fcc039
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-26 18:34:30 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,22 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
   # code here
+  first_name_of_winner = ""
+  data[season].each do |x|
+    if x.values.include?("Winner")
+      first_name_of_winner = (x["name"].split(" "))[0]
+      
+    end 
+  end 
+  
+  first_name_of_winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  
+  data.values.each{|x| x.each{|y|  y["occupation"] == occupation ? name = y["name"]}}
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
